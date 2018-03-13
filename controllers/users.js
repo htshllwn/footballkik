329de2611630e99c89c2293c948d440fb80ef392
Hitesh Lalwani <htshllwn@gmail.com>
2018-03-13 13:41:37 +0530

    Added Local Login/Signup

---
+++ controllers/users.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = function(_, passport, User){
+
+    return {
+        SetRouting: function(router){
+            router.get('/', this.indexPage);
+            router.get('/signup', this.getSignUp);
+            router.get('/home', this.homePage);
+
+
+            router.post('/', User.LoginValidation, this.postLogin);
+            router.post('/signup', User.SignUpValidation, this.postSignUp);
+        },
+
+        indexPage: function(req, res){
+            const errors = req.flash('error');
+            return res.render('index', {
+                title: 'Football | Login',
+                messages: errors,
+                hasErrors: errors.length > 0
+            });
+        },
+
+        getSignUp: function(req, res){
+            const errors = req.flash('error');
+            return res.render('signup', {
+                title: 'Football | SignUp',
+                messages: errors,
+                hasErrors: errors.length > 0
+            });
+        },
+
+        postLogin: passport.authenticate('local.login', {
+            successRedirect: '/home',
+            failureRedirect: '/',
+            failureFlash: true
+        }),
+
+        postSignUp: passport.authenticate('local.signup', {
+            successRedirect: '/home',
+            failureRedirect: '/signup',
+            failureFlash: true
+        }),
+
+       
+
+        homePage: function(req, res){
+            return res.render('home');
+        },
+    }
+
+}
